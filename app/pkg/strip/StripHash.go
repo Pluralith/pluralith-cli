1871414663e654e8ab9a3c6f723247dce43926e8
Daniel Putzer <danielputzer.work@gmail.com>
2022-02-07 23:05:04 +0100

    fix: Adjust plan and output paths

--- app/pkg/strip/StripHash.go
+++ app/pkg/strip/StripHash.go
@@ -248,8 +248,8 @@ func (S *StripState) StripAndHash() error {
 	stripSpinner.Start()
 
 	// Initialize relevant paths
-	planPath := filepath.Join(auxiliary.PathInstance.WorkingPath, "state", "pluralith.state.stripped")
-	outPath := filepath.Join(auxiliary.PathInstance.WorkingPath, "state", "pluralith.state.hashed")
+	planPath := filepath.Join(auxiliary.PathInstance.WorkingPath, "pluralith.state.stripped")
+	outPath := filepath.Join(auxiliary.PathInstance.WorkingPath, "pluralith.state.hashed")
 
 	// Check if plan state exists -> if not, alert and return
 	if _, existErr := os.Stat(planPath); existErr != nil {
