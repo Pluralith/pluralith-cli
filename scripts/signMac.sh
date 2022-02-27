5b8ef3f04fe28c7febfa5acd3249ea73e6715b22
Daniel Putzer <danielputzer.work@gmail.com>
2022-02-28 00:45:04 +0100

    fix: Adjust gon config

--- scripts/signMac.sh
+++ scripts/signMac.sh
@@ -1,16 +1,17 @@
 # 1) Build unsigned binary
-cd ../app
-env GOOS=darwin GOARCH=amd64 go build -o ../dist/unsigned/pluralith_cli_darwin_amd64
-env GOOS=linux GOARCH=amd64 go build -o ../dist/unsigned/pluralith_cli_linux_amd64
-env GOOS=windows GOARCH=amd64 go build -o ../dist/unsigned/pluralith_cli_windows_amd64
+# cd ../app
+# env GOOS=darwin GOARCH=amd64 go build -o ../dist/unsigned/pluralith_cli_darwin_amd64
+# env GOOS=linux GOARCH=amd64 go build -o ../dist/unsigned/pluralith_cli_linux_amd64
+# env GOOS=windows GOARCH=amd64 go build -o ../dist/unsigned/pluralith_cli_windows_amd64
 
 # 2) Sign & notarize binary
-cd ..
+# cd ..
 gon gon-config.json
 
 # 3) Unzip notarized binary
-cd dist/signed
-unzip -o *.zip
+cd dist/darwin_darwin_amd64
+rm pluralith
+unzip -o pluralith.zip
 
 # 4) Eliminate zips
 rm *.zip
