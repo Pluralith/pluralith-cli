48d0a4d2da7729f6afbf7f0ad37732c253a6cd81
Daniel Putzer <danielputzer.work@gmail.com>
2022-03-08 19:58:57 +0100

    chore: Refactor CI export

--- app/pkg/graph/ExportDiagram.go
+++ app/pkg/graph/ExportDiagram.go
@@ -7,8 +7,34 @@ import (
 	"os/exec"
 	"path/filepath"
 	"pluralith/pkg/auxiliary"
+	"pluralith/pkg/ci"
 	"pluralith/pkg/ux"
 )
+
+func HandlePRUpdate(diagramPath string) error {
+	functionName := "preparePRComment"
+
+	// Upload diagram to storage for pull request comment hosting
+	urls, hostErr := HostExport(diagramPath)
+	if hostErr != nil {
+		return fmt.Errorf("hosting diagram for PR comment failed -> %v: %w", functionName, hostErr)
+	}
+
+	// Generate pull request comment markdown
+	commentMD, commentErr := ci.GeneratePRComment(urls)
+	if commentErr != nil {
+		return fmt.Errorf("hosting diagram for PR comment failed -> %v: %w", functionName, hostErr)
+	}
+
+	fmt.Println(commentMD)
+
+	// Write markdown to file system for usage by pipeline
+	// if writeErr := os.WriteFile("commend.md", []byte(commentMD), 0700); writeErr != nil {
+	// 	return fmt.Errorf("writing PR comment markdown to filesystem failed -> %v: %w", functionName, hostErr)
+	// }
+
+	return nil
+}
 
 func ExportDiagram(diagramValues map[string]interface{}) error {
 	functionName := "ExportDiagram"
@@ -49,7 +75,16 @@ func ExportDiagram(diagramValues map[string]interface{}) error {
 	}
 
 	exportPath := filepath.Join(diagramValues["OutDir"].(string), diagramValues["FileName"].(string)) + ".pdf"
-	LogExport(exportPath)
+
+	if auxiliary.StateInstance.IsCI {
+		if prErr := HandlePRUpdate(exportPath); prErr != nil {
+			return fmt.Errorf("handling pull request update failed -> %v: %w", functionName, prErr)
+		}
+	} else {
+		if logErr := LogExport(); logErr != nil {
+			return fmt.Errorf("logging diagram export failed -> %v: %w", functionName, logErr)
+		}
+	}
 
 	exportSpinner.Success()
 	ux.PrintFormatted("  → ", []string{"blue"})
