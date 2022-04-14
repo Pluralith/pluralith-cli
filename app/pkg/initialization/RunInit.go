731c233ae8ebaa8af4137c44b95481881c435e8a
Daniel Putzer <danielputzer.work@gmail.com>
2022-04-14 17:39:21 +0200

    fix: Rework authentication in init command

--- app/pkg/initialization/RunInit.go
+++ app/pkg/initialization/RunInit.go
@@ -9,10 +9,6 @@ import (
 func RunInit(isEmpty bool, APIKey string, projectId string) error {
 	functionName := "RunInit"
 
-	if !isEmpty && projectId == "" {
-		fmt.Println("Lets set up your project and get you up and running.\n")
-	}
-
 	if isEmpty {
 		fmt.Println()
 		if writeErr := WriteConfig(projectId); writeErr != nil {
@@ -21,40 +17,48 @@ func RunInit(isEmpty bool, APIKey string, projectId string) error {
 		return nil
 	}
 
+	ux.PrintFormatted("\n→", []string{"blue", "bold"})
+	ux.PrintFormatted(" Authentication\n", []string{"white", "bold"})
+
 	if APIKey == "" {
-		ux.PrintFormatted("⠿ ", []string{"blue"})
+		ux.PrintFormatted("  ⠿ ", []string{"blue"})
 		fmt.Println("We noticed you are not authenticated!")
-		ux.PrintFormatted("→", []string{"blue", "bold"})
+		ux.PrintFormatted("  →", []string{"blue", "bold"})
 		fmt.Print(" Enter your API Key: ")
 
 		// Capture user input
 		fmt.Scanln(&APIKey)
-		loginValid, loginErr := auth.RunLogin(APIKey)
-		if !loginValid {
-			return nil
-		}
-		if loginErr != nil {
-			return fmt.Errorf("failed to authenticate -> %v: %w", functionName, loginErr)
-		}
 	}
 
+	// Run login routine and set credentials file
+	loginValid, loginErr := auth.RunLogin(APIKey)
+	if !loginValid {
+		return nil
+	}
+	if loginErr != nil {
+		return fmt.Errorf("failed to authenticate -> %v: %w", functionName, loginErr)
+	}
+
+	ux.PrintFormatted("→", []string{"blue", "bold"})
+	ux.PrintFormatted(" Project\n", []string{"white", "bold"})
+
 	if projectId == "" {
-		ux.PrintFormatted("→", []string{"blue", "bold"})
+		ux.PrintFormatted("  →", []string{"blue", "bold"})
 		fmt.Print(" Enter Project Id: ")
 
 		// Capture user input
 		fmt.Scanln(&projectId)
-
-		projectValid, projectErr := auth.VerifyProject(projectId)
-		if !projectValid {
-			return nil
-		}
-		if projectErr != nil {
-			return fmt.Errorf("failed to verify project id -> %v: %w", functionName, projectErr)
-		}
-
-		fmt.Print("  ") // Formatting gimmick
 	}
+
+	projectValid, projectErr := auth.VerifyProject(projectId)
+	if !projectValid {
+		return nil
+	}
+	if projectErr != nil {
+		return fmt.Errorf("failed to verify project id -> %v: %w", functionName, projectErr)
+	}
+
+	fmt.Print("  ") // Formatting gimmick
 
 	if writeErr := WriteConfig(projectId); writeErr != nil {
 		return fmt.Errorf("failed to create config template -> %v: %w", functionName, writeErr)
