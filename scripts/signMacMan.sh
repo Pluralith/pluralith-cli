08c10a259b4afe4457296150df2cd0d1d69d89a2
Daniel Putzer <danielputzer.work@gmail.com>
2022-07-02 16:08:27 +0200

    chore: Update manual sign script

--- scripts/signMacMan.sh
+++ scripts/signMacMan.sh
@@ -2,11 +2,5 @@ cd app
 env GOOS=darwin GOARCH=amd64 go build -o ../dist/darwin_darwin_amd64/pluralith
 cd ..
 
-# 1) Sign & notarize binary
+# Sign & notarize binary
 gon gon-config.json
-
-# 2) Replace unsigned with signed binary
-cd dist/darwin_darwin_amd64
-rm pluralith
-unzip -o pluralith.zip
-rm *.zip
