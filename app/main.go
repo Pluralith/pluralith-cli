2fe9e4b5c6613fa32c8b4da6b99b50def4095cbe
Daniel Putzer <danielputzer.work@gmail.com>
2022-09-19 16:43:20 -0700

    fix: Switch to prod endpoints

--- app/main.go
+++ app/main.go
@@ -1,17 +1,5 @@
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Copyright © 2022 Pluralith Industries Inc. founders@pluralith.com
 */
 package main
 
@@ -27,7 +15,7 @@ func initApp() {
 	functionName := "initApp"
 
 	dblock.LockInstance.GenerateLock()
-	auxiliary.StateInstance.CLIVersion = "0.1.13-alpha"
+	auxiliary.StateInstance.CLIVersion = "0.1.13"
 
 	if pathGenErr := auxiliary.StateInstance.GeneratePaths(); pathGenErr != nil {
 		fmt.Println(fmt.Errorf("generating application paths failed -> %v: %w", functionName, pathGenErr))
